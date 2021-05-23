2dea49d5c7bf968f1c3f4046af6697a202082505
Alexander Zaikin <tandik242@gmail.com>
2021-05-23 20:16:49 +0500

    Initial state

---
+++ Mobile application/MathBattlegrounds/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MathBattlegrounds
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            ServerInfo.Authenticate();
+            ((Button)sender).Text = ServerInfo.Token;
+        }
+
+        void OnPlayButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnProfileButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+    }
+}
---
+++ Mobile application/MathBattlegrounds/ServerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+
+namespace MathBattlegrounds
+{
+    public static class ServerInfo
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public static string Token;
+
+        public static bool Authenticate()
+        {
+            var responseString = client.GetStringAsync("http://85.12.215.66:7777/api/authenticate?login=TEST&password=TEST");
+            Token = responseString.Result;
+            return true;
+        }
+
+    }
+}
