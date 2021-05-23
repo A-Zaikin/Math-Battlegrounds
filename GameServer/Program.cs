95dc8dca87675e1f52aa44a18df68d4365308e3c
lev <levndev@gmail.com>
2021-05-23 21:16:27 +0500

    Initial commit

---
+++ GameServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException();
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Any;
+                server = new TcpListener(localAddr, int.Parse(args[0]));
+
+                // запуск слушателя
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание подключений... ");
+
+                    // получаем входящее подключение
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Подключен клиент. Выполнение запроса...");
+
+                    // получаем сетевой поток для чтения и записи
+                    NetworkStream stream = client.GetStream();
+
+                    // сообщение для отправки клиенту
+                    string response = "Hello world";
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Отправлено сообщение: {0}", response);
+                    // закрываем поток
+                    stream.Close();
+                    // закрываем подключение
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+        }
+    }
+}
